7ece32d52c109ef6175fed7eeb59bb0fa52d05d8
nimonian <nimon@hotmail.co.uk>
2024-12-09 18:15:41 +0000

    solves a few

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from functools import reduce, cache
 
 
 def compose(*funcs):
@@ -7,3 +7,23 @@ def compose(*funcs):
         return lambda x: f(g(x))
 
     return reduce(_compose, funcs, lambda x: x)
+
+
+def prod(L):
+    return reduce(lambda x, y: x * y, L, 1)
+
+
+@cache
+def cartesian(lists):
+
+    if len(lists) == 1:
+        return tuple((a,) for a in lists[0])
+
+    A = lists[0]
+    B = cartesian(lists[1:])
+
+    return tuple((a,) + b for a in A for b in B)
+
+
+def filter(f, A):
+    return [a for a in A if f(a)]
