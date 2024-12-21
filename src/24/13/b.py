a1b4625ad89957fcdae2484e8d3942b691b47453
nimonian <nimon@hotmail.co.uk>
2024-12-21 01:35:41 +0000

    solves 13

---
+++ src/24/13/b.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import re
+
+
+def cost(game):
+    u1, u2, v1, v2, w1, w2 = map(int, re.findall(r"(\d+)", game))
+    a = (v2 * w1 - v1 * w2 + 10**13 * (v2 - v1)) / (u1 * v2 - v1 * u2)
+    b = (u1 * w2 - u2 * w1 + 10**13 * (u1 - u2)) / (u1 * v2 - v1 * u2)
+    return 3 * a + b if a == int(a) and b == int(b) else 0
+
+
+def solution():
+    path = Path(__file__).parent / "input.txt"
+    return sum(map(cost, path.read_text().strip().split("\n\n")))
