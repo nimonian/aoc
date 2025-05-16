ad7e553663676167108c261176b62bf60057bf7c
Simon Haines <nimon@hotmail.co.uk>
2025-05-17 00:44:15 +0100

    remove wasted assignment

--- src/20/05/a.py
+++ src/20/05/a.py
@@ -6,5 +6,4 @@ txt = Path(__file__).parent / "input.txt"
 def solution():
     lines = txt.read_text().splitlines()
     to_int = lambda s: sum(2**i * (c in ["B", "R"]) for i, c in enumerate(s[::-1]))
-    id = lambda s: to_int(s[:7]) * 8 + to_int(s[-3:])
-    return max(map(id, lines))
+    return max(map(lambda s: to_int(s[:7]) * 8 + to_int(s[-3:]), lines))
--- src/20/05/b.py
+++ src/20/05/b.py
@@ -7,11 +7,10 @@ txt = Path(__file__).parent / "input.txt"
 def solution():
     lines = txt.read_text().splitlines()
     to_int = lambda s: sum(2**i * (c in ["B", "R"]) for i, c in enumerate(s[::-1]))
-    id = lambda s: to_int(s[:7]) * 8 + to_int(s[-3:])
 
     a, b, s = inf, -inf, 0
     for line in lines:
-        n = id(line)
+        n = to_int(line[:7]) * 8 + to_int(line[-3:])
         a, b, s = min(a, n), max(b, n), s + n
 
     return (b + a) * (b - a + 1) // 2 - s
